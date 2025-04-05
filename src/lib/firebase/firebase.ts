04b0d8c3f394b76f406e012305d65fd3bdc39226
itojun-1230 <juntou1230@gmail.com>
2025-04-05 20:45:15 +0900

    feat: Firebase初期化ロジックを追加
    - Firebaseの初期化を行う`firebase.ts`ファイルを新規作成しました。
    - 環境変数を使用してFirebaseの設定を行っています。

---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp, type FirebaseOptions } from "firebase/app";
+
+const firebaseConfig: FirebaseOptions = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_API_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
