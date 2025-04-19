42e4feb58f30841293f74baf38773e960a3c3578
itojum <itojum1230@gmail.com>
2025-04-19 23:03:38 +0900

    feat: Jest設定を更新し、テスト環境を強化
    - jest.config.jsの設定を修正し、setupFilesAfterEnvをjest.setup.jsに変更
    - 新たにjest.setup.jsを作成し、グローバルなfetchのモックやNext.jsのRequest/Responseのモックを追加
    - テスト環境での環境変数を設定し、テストの柔軟性を向上

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,19 @@
-import nextJest from 'next/jest.js'
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
-})
+});
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
-}
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+};
 
-export default createJestConfig(customJestConfig) 
+module.exports = createJestConfig(customJestConfig); 
---
+++ jest.setup.js
@@ -0,0 +1,46 @@
+// @testing-library/jest-domの拡張機能をインポート
+import '@testing-library/jest-dom';
+
+// グローバルなfetchのモック
+global.fetch = jest.fn();
+
+// Next.jsのRequestとResponseのモック
+class MockHeaders {
+  constructor(init) {
+    Object.assign(this, init);
+  }
+}
+
+class MockRequest {
+  constructor(input, init = {}) {
+    this.url = input;
+    this.method = init.method || 'GET';
+    this.headers = new MockHeaders(init.headers || {});
+    this._body = init.body;
+  }
+
+  async json() {
+    return JSON.parse(this._body);
+  }
+}
+
+class MockResponse {
+  constructor(body, init = {}) {
+    this._body = typeof body === 'string' ? body : JSON.stringify(body);
+    this.status = init.status || 200;
+    this.headers = new MockHeaders(init.headers || {});
+    this.ok = this.status >= 200 && this.status < 300;
+  }
+
+  async json() {
+    return JSON.parse(this._body);
+  }
+}
+
+global.Request = MockRequest;
+global.Response = MockResponse;
+global.Headers = MockHeaders;
+
+// テスト環境での環境変数の設定
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://test.supabase.co';
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'test-anon-key'; 
